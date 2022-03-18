59ffa030c88ff74e4ecbeea8f5215e00c7e6444e
Anurag Kumar <anurag.kumar@ARMSTRONGLTD.LOCAL>
2022-03-18 17:57:52 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../src/components/Home';
 import Barcode from './components/Barcode/Barcode';
 import SetConfiguration from './components/SetConfiguration';
@@ -7,15 +7,15 @@ import PalletRegister from './components/PalletRegister';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/barcode" element={<Barcode />} />
         <Route path="/InboundSimulation" element={<InBoundSimulation />} />
         <Route path="/pallet-register" element={<PalletRegister />} />
         <Route path="/set-configuration" element={<SetConfiguration />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
